9dd7ae23dba6459c79fc4f77ba43b94e5a5beda6
Your Name <you@example.com>
2023-01-29 15:27:36 +0530

    Some changes

--- NumPy arrays.py
+++ NumPy arrays.py
@@ -1,5 +1,12 @@
 
-name = ("is my name")
-print("yash " +name)
-a = [1,2,4,5,6,7]
+# inport numpy 
+import numpy as np
+a = np.array([1,2,3])
 print(a)
+
+a1 = np.zeros(3)
+print(a1)
+
+print(np.ones((4,5)))
+
+print(np.full((4,5), 69))
